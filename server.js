9b0870124b2d25edfb30cd128184b41b35f60959
Brett B <65505937+BreadBooks@users.noreply.github.com>
2020-11-04 14:01:04 -0600

    Fixed-Link

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 
 const db = require("./models");
-const PORT = process.env.PORT || 4100;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static("public"));
